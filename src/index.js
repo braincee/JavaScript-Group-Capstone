7edea40908bb032573ce642258a4f291736aa74f
braincee <kwesi938@gmail.com>
2022-05-17 15:36:45 +0100

    Popup API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
+
 import './style.css';
-
- const movieUrl = "https://api.tvmaze.com/search/shows?q=die";
-
- fetch(movieUrl)
- .then(response => response.json())
- .then(data => console.log(data));
