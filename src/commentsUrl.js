31b5c068584fb5c10ac769b15cd9b18f40108719
ramrod433 <djoletoamanor@gmail.com>
2022-05-19 10:23:20 +0000

    Added new comment

--- src/commentsUrl.js
+++ src/commentsUrl.js
@@ -1,4 +1,14 @@
 export default class commentsUrl {
+    static commentsUrl =
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/UUC27nnzGoxTuyIJisKA/comments';
+
+  static getComments = async (id) => {
+    const response = await fetch(`${this.commentsUrl}?item_id=${id}`);
+
+    const data = await response.json();
+    return data;
+  };
+
     static setComments = async (id, username, comment) => {
       const response = await fetch(this.commentsUrl, {
         method: 'POST',
